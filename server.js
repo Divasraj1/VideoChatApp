54726a4c14ef3c49126773171c00ac7180575f83
Divasraj1 <Divasraj1>
2024-02-04 10:21:56 +0530

    added cors

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const server = require("http").Server(app);
 app.set('view engine','ejs')
 app.use(express.static('public'))
 const {v4:uuidv4} = require("uuid")
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
 const {ExpressPeerServer} = require("peer");
 const peerServer = ExpressPeerServer(server,{
     debug: true
